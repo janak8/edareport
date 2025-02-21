248eebe873bc85fa327a24f0c3b5241c3c424247
Janak Adhikari <135828134+janak8@users.noreply.github.com>
2025-02-21 22:59:09 +0545

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load data
-df = pd.to_excel('Opportunity Wise Data.csv')
+df = pd.to_excel('OpportunityWiseData2.xlsx')
 print(f"Initial rows: {len(df)}")
 
 # 1. Explore the Dataset
